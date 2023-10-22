1a707c8a448ac1c7a0adfa35d30e60d16906107f
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-10-22 15:39:56 +0600

    added navbar and footer section

--- src/Layout/Root/Root.jsx
+++ src/Layout/Root/Root.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../../Sheare/Navbar/Navbar";
 
 const Root = () => {
     return (
         <div className="max-w-6xl mx-auto px-5">
+            <Navbar></Navbar>
             <Outlet></Outlet>
         </div>
     );
