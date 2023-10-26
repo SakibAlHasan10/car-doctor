cacdcce4c6ca4658e1039cf450cd75417197b125
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-10-26 11:59:57 +0600

    added create user, signIn user and user state change

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,11 +1,41 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { app } from "../Firebase/firebase.config";
 export const AuthContext = createContext();
+const auth = getAuth(app)
+
 const AuthProvider = ({ children }) => {
   const [theme, setTheme] = useState(true);
+  const [user, setUser] = useState(null)
+  const [loader, setLoader] = useState(true)
+  // create user with email
+  const createUserWithEmail =(email, password)=>{
+    setLoader(true);
+    return createUserWithEmailAndPassword(auth, email, password)
+  }
+  // signIn with email
+  const signInWithEmail =(email, password)=>{
+    setLoader(true)
+    signInWithEmailAndPassword(auth, email, password)
+  }
+  // user state change 
+  useEffect(()=>{
+    const unSubscribe = onAuthStateChanged(auth, currentUser=>{
+      setUser(currentUser)
+    })
+    return()=>{
+      unSubscribe()
+    }
+  },[])
+  // auth value
   const authInfo = {
     theme,
+    user,
+    loader,
     setTheme,
+    createUserWithEmail,
+    signInWithEmail,
   };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
